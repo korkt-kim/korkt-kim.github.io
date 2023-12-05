6fc11c259ce50fa32e0873e8704725769efb2582
korkt-kim <gnscott6@naver.com>
2023-12-06 02:45:55 +0900

    fix(@zakelstorm/blog): revalidation 수정

--- apps/blog/app/action.ts
+++ apps/blog/app/action.ts
@@ -1,23 +1,22 @@
 'use server'
 
+import { revalidateTag } from 'next/cache'
+
 import { env } from '@/env'
 
-export async function onClick() {
+export async function createArticle() {
   const res = await fetch(`${env('NEXT_PUBLIC_BASE_URL')}/api/article`, {
     body: JSON.stringify({
-      title: 'a',
+      title: 'k',
       password: 'rnrn0808!!',
       category: ['Typescript'],
     }),
     method: 'POST',
+    cache: 'no-store',
   })
 
   const data = await res.json()
-
-  await fetch(`${env('NEXT_PUBLIC_BASE_URL')}/api/revalidate`, {
-    body: JSON.stringify(data),
-    method: 'POST',
-  })
+  revalidateTag('article')
 
   return JSON.stringify(data)
 }
