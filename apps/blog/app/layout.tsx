8bd35b3841b73503d28bf325c1b91b83cd3ed92f
korkt-kim <gnscott6@naver.com>
2024-01-28 19:40:15 +0900

    feat(@zakelstorm/blog): Google Analytistic 적용

--- apps/blog/app/layout.tsx
+++ apps/blog/app/layout.tsx
@@ -5,10 +5,14 @@ import 'react-loading-skeleton/dist/skeleton.css'
 import { ToastCtx } from '@zakelstorm/ui'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import Head from 'next/head'
+import Script from 'next/script'
 import { PropsWithChildren } from 'react'
 
 import { AppShell } from '@/features/layouts/AppShell'
 import { BasicPageTemplate } from '@/templates/BasicPageTemplate'
+
+import * as gtag from '../libs/gtag'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -27,7 +31,26 @@ export default function RootLayout({
 }: PropsWithChildren): JSX.Element {
   return (
     <html lang='en'>
+      <Head>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+
+              gtag('config', '${gtag.GA_TRACKING_ID}', {
+                page_path: window.location.pathname,
+              });
+            `,
+          }}
+        />
+      </Head>
       <body className={inter.className}>
+        <Script
+          strategy='afterInteractive'
+          src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
+        />
         <AppShell>
           <BasicPageTemplate>
             <BasicPageTemplate.Content>{children}</BasicPageTemplate.Content>
