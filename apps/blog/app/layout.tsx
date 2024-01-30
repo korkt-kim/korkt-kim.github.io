448f2333a77e4f42201e40b6fea47a6e1bcc4160
korkt-kim <gnscott6@naver.com>
2024-01-31 08:59:47 +0900

    fix(@zakelstorm/blog): framer lazy import & lazyMotion 적용

--- apps/blog/app/layout.tsx
+++ apps/blog/app/layout.tsx
@@ -1,15 +1,14 @@
 import '@/styles/globals.css'
-import '@zakelstorm/ui/globals.css'
-import 'react-loading-skeleton/dist/skeleton.css'
 
 import { GoogleAnalytics } from '@next/third-parties/google'
 import { ToastCtx } from '@zakelstorm/ui'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
-import { PropsWithChildren } from 'react'
+import type { PropsWithChildren } from 'react'
 
 import { env } from '@/env'
 import { AppShell } from '@/features/layouts/AppShell'
+import { domAnimation, LazyMotion } from '@/libs/framer'
 import { BasicPageTemplate } from '@/templates/BasicPageTemplate'
 
 const inter = Inter({ subsets: ['latin'] })
@@ -34,11 +33,13 @@ export default function RootLayout({
       </head>
 
       <body className={inter.className}>
-        <AppShell>
-          <BasicPageTemplate>
-            <BasicPageTemplate.Content>{children}</BasicPageTemplate.Content>
-          </BasicPageTemplate>
-        </AppShell>
+        <LazyMotion features={domAnimation}>
+          <AppShell>
+            <BasicPageTemplate>
+              <BasicPageTemplate.Content>{children}</BasicPageTemplate.Content>
+            </BasicPageTemplate>
+          </AppShell>
+        </LazyMotion>
         <ToastCtx />
       </body>
       <GoogleAnalytics gaId={env('NEXT_PUBLIC_GA_ID')} />
--- apps/blog/libs/framer.ts
+++ apps/blog/libs/framer.ts
@@ -1,5 +1,13 @@
 'use client'
 
-import { motion } from 'framer-motion'
+import dynamic from 'next/dynamic'
 
-export const MotionDiv = motion.div
+export const MotionDiv = dynamic(() =>
+  import('framer-motion').then(mod => mod.m.div)
+)
+
+export const LazyMotion = dynamic(() =>
+  import('framer-motion').then(mod => mod.LazyMotion)
+)
+
+export { domAnimation } from 'framer-motion'
